0d817b9934ae5cf4afeca12c6e24b2c79b6d33c8
Ashraf Qopiah <203550258+ashrafmo-2@users.noreply.github.com>
2026-07-07 12:47:42 +0300

    Configure image domains for backend and localhost
    Add remote image pattern configurations for lms-backend.elmotechsoft.online (HTTPS) and localhost (HTTP) to enable image loading from these domains.

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "ui-avatars.com",
       },
+      {
+        protocol: "https",
+        hostname: "lms-backend.elmotechsoft.online",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
     ],
   },
 };
